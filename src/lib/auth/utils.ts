7029c05a347c772f50c642329d74a70e665904d7
miguelalcalde <miguel.alcalde@vercel.com>
2024-05-07 18:51:43 +0200

    fix typing errors

--- src/lib/auth/utils.ts
+++ src/lib/auth/utils.ts
@@ -38,6 +38,7 @@ export const authOptions: NextAuthOptions = {
     EmailProvider({
       server: {
         host: process.env.EMAIL_SERVER_HOST,
+        // @ts-expect-error
         port: process.env.EMAIL_SERVER_PORT,
         auth: {
           user: process.env.EMAIL_SERVER_USER,
